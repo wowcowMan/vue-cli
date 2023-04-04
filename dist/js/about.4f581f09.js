80f9ac27980f01cc64701914dddf08852f61459a
John Peng <89276080john@gmail.com>
2023-04-04 15:42:59 +0800

    test

---
+++ dist/js/about.4f581f09.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkstore_project"]=self["webpackChunkstore_project"]||[]).push([[443],{7381:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var r=n(3396);const s={class:"about"},u=(0,r._)("h1",null,"This is an about page",-1),a=[u];function c(t,e){return(0,r.wg)(),(0,r.iD)("div",s,a)}var o=n(89);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.4f581f09.js.map
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: { allowedHosts: 'all', },
+
+  pluginOptions: {
+    moment: {
+      locales: [
+        ''
+      ]
+    }
+  },
 })
